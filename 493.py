97fbadc85422d5e141d838f3168321eb1ad4ed13
dgcarneiro <dcarneiro1985@gmail.com>
2016-01-23 23:22:43 -0500

    Update 493.py

--- 493.py
+++ 493.py
@@ -129,34 +129,6 @@ def gps():
 			print NMEA2
 			print NMEA3
 
-def get_img(self):
-		#update current image.
-		while 1:
-			ret, frame = self.cap.read()
-			if ret==True:#check if cap.read() returned frame
-				self.img=frame
-				break
-	def get_lines(self):
-		#function that returns a series of lines in the current image. Possibly using an input region of interest of the image.
-		pass
-	def wh_det(img,x1=10,x2=640,y1=470,y2=0,xit=10,yit=-10):
-		tot=0;
-		l1=[]
-		for x in range(x1,x2,xit):
-			test=3#used to ignore noise in the image. if the values goes too low the system stops incrementing in that y direction
-			for y in range(y1,y2,yit):
-				bgr=self.img[y,x]
-				if abs(int(bgr[1])-int(bgr[2]))<14 and ((bgr[0]*1.25)>(bgr[1]+bgr[2])/2):#works
-					tot+=1
-					if test<3:
-						test+=1
-					else:
-						test-=1
-					if test<2:
-						break
-			l1.append(ytot)
-		return l1
-
 def forward():
 	GPIO.output(right_wheel_dir, GPIO.HIGH)
 	GPIO.output(left_wheel_dir, GPIO.HIGH)
